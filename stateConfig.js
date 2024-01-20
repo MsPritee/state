6bdfd54057716ca55d43b6f0613f03d586bcaa6b
MsPritee <125807173+MsPritee@users.noreply.github.com>
2024-01-20 22:11:45 +0530

    Add files via upload

---
+++ stateConfig.js
@@ -0,0 +1,20 @@
+const stateConfig = [
+  {
+    name: "Select",
+    path: "",
+  },
+  {
+    name: "Maharashtra",
+    path: "./MH.json",
+  },
+  {
+    name: "Gujarat",
+    path: "./GJ.json",
+  },
+  {
+    name: "Madhya Pradesh",
+    path: "./MP.json",
+  },
+];
+
+export default stateConfig;
